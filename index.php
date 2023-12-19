c3888cf782c3e41cefa7c5504afb8918a9348bd8
nk246 <97559302+nk246@users.noreply.github.com>
2023-12-19 17:25:54 +0100

    .

--- index.php
+++ index.php
@@ -10,7 +10,21 @@
 </head>
 <body>
     <div id="menu">
-        <div id="logo" href="index.php"> <img src="" alt="NK Logo"></div>
+        <div id="logo" href="index.php"> 
+            <?xml version="1.0" encoding="utf-8"?>
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 45.7 20.2" style="enable-background:new 0 0 45.7 20.2;" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#FFFFFF;}
+</style>
+<g>
+	<rect x="15" y="17.8" class="st0" width="16.4" height="2.4"/>
+	<path class="st0" d="M0,20.2h3.2v-7.6c0-1.5,0.7-2.5,2.7-2.5c2,0,2.7,1,2.7,2.6v7.5h3.2v-7.8c0-3.2-3.2-5-5.9-5
+		c-2.7,0-5.9,1.8-5.9,5L0,20.2z"/>
+	<polygon class="st0" points="42.3,7.7 37.9,13.8 42.6,20.2 45.7,20.2 45.7,20 41,13.8 45.3,7.8 45.3,7.7 	"/>
+	<rect x="34.7" class="st0" width="3.2" height="20.2"/>
+</g>
+</svg></div>
         <div id="menu-items">
             <div class="menu-item">About Me</div>
             <div class="menu-item" id="projects">Projects</div>
