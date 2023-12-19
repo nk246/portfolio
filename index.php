c98dfd360bd609b8d4466722692801829524f110
nk246 <97559302+nk246@users.noreply.github.com>
2023-12-19 17:37:51 +0100

    .

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <div id="menu">
-        <div id="logo" href="index.php"> <img src="media/logo/Logo.svg" alt="NK Logo"></div>
+        <div id="logo" href="index.php"> <img src="media/logo/Logo.svg" alt="NK Logo" max-width="70%"></div>
         <div id="menu-items">
             <div class="menu-item">About Me</div>
             <div class="menu-item" id="projects">Projects</div>
