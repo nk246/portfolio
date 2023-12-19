dc20c90f2d036663f1cc8c70a2f137b2461f1078
nk246 <97559302+nk246@users.noreply.github.com>
2023-12-19 18:22:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+
 // Get the requested URL
 $requestUri = $_SERVER['REQUEST_URI'];
 
@@ -12,12 +14,17 @@ switch ($requestUri) {
     case '/about':
         include 'about.php';
         break;
-    case '/projects/${projectId}':
-        include '{projectId}.php';
-        break;
-
     default:
-        // Handle 404
-        include '404.php';
+        // Check if the URL matches the /projects/ pattern
+        if (preg_match('/\/projects\/(\d+)/', $requestUri, $matches)) {
+            // Extract the project ID from the matched pattern
+            $projectId = $matches[1];
+            // Include the corresponding PHP file
+            include "project.php";
+        } else {
+            // Handle 404
+            include '404.php';
+        }
         break;
 }
+?>
